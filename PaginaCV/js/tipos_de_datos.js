00118035a722c639d2c70c1d84f9c865197ddef3
Israel Díaz Ruiz <diaz.ruiz.israel.a@gmail.com>
2026-04-19 19:55:49 -0600

    Hola mundo en rewards, y describir los tipos de datos que acepta como tal el lenguaje

---
+++ PaginaCV/js/tipos_de_datos.js
@@ -0,0 +1,14 @@
+
+//Tipos de Datos en Javascript
+let numero1 = 42
+let numeroletra = "Veinticinco"
+let numeronegativo = -666
+let booleano = true 
+let cero = 0
+let comillas = "\""
+let nulo = null
+let indefinido = ""
+let booleanofalso = false
+
+// imprimir en la consola
+console.log(numero1, numeroletra, numeronegativo, booleano, cero, comillas, nulo, indefinido, booleanofalso)
