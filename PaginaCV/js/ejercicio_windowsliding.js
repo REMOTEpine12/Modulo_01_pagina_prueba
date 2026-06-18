aeef9ae0a36ca9b37ac533718c0c6ec53666af3c
Israel Díaz Ruiz <diaz.ruiz.israel.a@gmail.com>
2026-06-17 21:58:46 -0600

    Agregar archivo ejercicio_windowsliding.js con arreglo de palabras y función WSW

---
+++ PaginaCV/js/ejercicio_windowsliding.js
@@ -0,0 +1,8 @@
+arraypalabras = ["hola", "adios", "casa", "perro", "gato", "ventana", "puerta", "coche", "bicicleta", "avion"];
+
+
+function WSW(arraypalabras, k) {
+    let resultado = [];
+    
+
+}
